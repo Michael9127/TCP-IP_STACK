c881764b468aec13829ff18cea3f215e5d80dadf
Michael Borkenstein <mborkenstein@vectra.ai>
2018-08-14 04:34:32 +0000

    print out ethernet contents

--- src/parse_protocol.c
+++ src/parse_protocol.c
@@ -3,8 +3,21 @@
 
 void print_ethernet(char *buffer, int nread){
     struct eth_hdr *hdr = (struct eth_hdr *) buffer;
-    printf("%012x\n", hdr->dmac);
-
+    printf("DST: %02x:%02x:%02x:%02x:%02x:%02x SRC: %02x:%02x:%02x:%02x:%02x:%02x ethertype: %d\n", 
+            hdr->dmac[0],
+            hdr->dmac[1],
+            hdr->dmac[2],
+            hdr->dmac[3],
+            hdr->dmac[4],
+            hdr->dmac[5],
+            hdr->smac[0],
+            hdr->smac[1],
+            hdr->smac[2],
+            hdr->smac[3],
+            hdr->smac[4],
+            hdr->smac[5],
+            hdr->ethertype
+            );
 
 }
 
